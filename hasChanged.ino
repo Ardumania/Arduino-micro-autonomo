067210ce77d0d75a550ac5a306ca72c03beb0787
Jose Manuel Escuder Martinez <josemescuder@gmail.com>
2020-01-19 16:21:22 +0100

    Add files via upload

---
+++ hasChanged.ino
@@ -0,0 +1,17 @@
+boolean hasChanged(int p){
+  actualState = digitalRead(p);
+
+  if (lastState != actualState)  // there was a chage in p state
+  {
+    counter++;                   // count state changes
+    int isPair = counter % 2;    // only want pair numbers
+    if (isPair != 1)              
+    {
+      lastState = actualState;
+      return true;
+    }else{
+      lastState = actualState;
+      return false;
+    }
+  }else return false;
+}
